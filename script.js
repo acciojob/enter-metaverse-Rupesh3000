8601eb5edb7eaa4be704d572d3743e285a59e9c1
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2026-01-12 00:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const btn = document.getElementById("enterBtn");
 btn.addEventListener("click", () => {
   const newH1 = document.createElement("h1");
   newH1.innerHTML = "Entered Metaverse";
-
+newH1.id = p.id;
   p.parentNode.replaceChild(newH1, p);
-p.id = "status"
+
 });
