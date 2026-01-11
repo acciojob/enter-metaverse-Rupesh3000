18cc68e998885453a90dd8944b67ecaa8b83f564
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2026-01-12 00:30:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const p = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
 
-const btn = document.getElementById("enterBtn");
+// Add an event listener to the button
+enterButton.addEventListener("click", function () {
+  const currentStatusElement = document.getElementById("status");
 
-btn.addEventListener("click", () => {
-  const newH1 = document.createElement("h1");
-  newH1.innerHTML = "Entered Metaverse";
-newH1.id = p.id;
-  p.parentNode.replaceChild(newH1, p);
+  if (currentStatusElement) {
+    const newStatusElement = document.createElement("h1");
 
+    newStatusElement.textContent = "Entered Metaverse";
+
+    newStatusElement.id = "status";
+
+    currentStatusElement.replaceWith(newStatusElement);
+  }
 });
